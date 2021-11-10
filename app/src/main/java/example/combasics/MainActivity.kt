bc93a67468585964716ef77030e91299e7f28571
MobDe <MobDevX.outlook.com>
2021-11-10 20:40:22 +0700

    OutlinedButton

--- app/src/main/java/example/combasics/MainActivity.kt
+++ app/src/main/java/example/combasics/MainActivity.kt
@@ -3,12 +3,14 @@ package example.combasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,10 +32,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(text = "Hello,")
-            Text(text = name)
+    val names: List<String> = listOf("World", "Compose")
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Column {
+        for (name in names) {
+            Surface(
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+            ) {
+                Row(modifier = Modifier.padding(24.dp)) {
+                    Column(modifier = Modifier
+                        .weight(1f)
+                        .padding(bottom = extraPadding)
+                    ) {
+                        Text(text = "Hello, ")
+                        Text(text = name)
+                    }
+                    OutlinedButton(
+                        onClick = { expanded.value = !expanded.value  }
+                    ) {
+                        Text(if (expanded.value) "Show less" else "Show more")
+                    }
+                }
+            }
         }
     }
 }
